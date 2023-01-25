77b5d75fae9bee749c4f2d37a702d50736c97e80
Zeshan Butt <Zeshan.butt3331@gmail.com>
2023-01-25 19:13:09 +0500

    Broken our homepage into components and syled our components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import './homepage_style.scss';
+import Directory from '../../components/directory/directory'
+const Homepage = () => {
+  return (
+    <div className="homepage">
+        <Directory/>
+    </div>
+  )
+}
+
+export default Homepage
